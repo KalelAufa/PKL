29abb53a28d687ba0e5d7f5e6627d8280fb7214a
KalelAufa <mkalelaufa@gmail.com>
2026-04-07 10:01:40 +0700

    .

--- APRIL/WebTNS-RF/extract.php
+++
@@ -1,9 +0,0 @@
-<?php
-$out = ["kategori" => [], "produk" => []];
-$old = "c:/laragon/www/PKL/APRIL/WebTNS/";
-
-$hc = file_get_contents($old . "app/Http/Controllers/HomeController.php");
-if (preg_match("/\[\s*\[(.*?)\]\s*\]/is", $hc, $m)) {
-    // Actually, I will write it all manually by having php do reflection or eval.
-}
-
--- APRIL/WebTNS-RF/routes/web.php
+++ APRIL/WebTNS-RF/routes/web.php
@@ -3,13 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KatalogController;
 
-// Route Home (Welcome)
-Route::get("/", function () {
-    return view("welcome");
-});
-
 // Route Utama (Dashboard Kategori)
-Route::get("/home", function () {
+Route::get("/", function () {
     $products = [];
     foreach (config("katalog.kategori") as $slug => $data) {
         $products[] = [
